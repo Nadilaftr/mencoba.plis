eb3977800d0c37ab9d2cc8a076f26f1d54285e45
Nadilaftr <118698890+Nadilaftr@users.noreply.github.com>
2023-09-12 22:01:06 +0700

    Create pythonyach.py
    editan awal

---
+++ pythonyach.py
@@ -0,0 +1,7 @@
+aku = input("tolong masukkan suka atau tidak suka: ")
+dia = input("tolong masukkan suka atau tidak; ")
+
+if aku == dia :
+  print("SELAMAT KALIAN BAHAGIA")
+else :
+  print("Maaf yah, ada yang perasaannya tidak sejalan")
