f2a87446ebfbef800f35aa70a89108468667dbc4
Nadilaftr <118698890+Nadilaftr@users.noreply.github.com>
2023-09-12 22:14:33 +0700

    Update pythonyach.py
    penambahan baru

--- pythonyach.py
+++ pythonyach.py
@@ -4,4 +4,5 @@ dia = input("tolong masukkan suka atau tidak; ")
 if aku == dia :
   print("SELAMAT KALIAN BAHAGIA")
 else :
-  print("Maaf yah, ada yang perasaannya tidak sejalan")
+  print("Maaf yah, ada yang perasaannya tidak sejalan semoga dilain waktu bisa bertemu yang lebih baik")
+
